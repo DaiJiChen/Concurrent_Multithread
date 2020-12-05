f3b034da067d1cc4da2aa6c1aa3cecca568115bf
DaiJichen <1245487099@qq.com>
2020-12-05 01:01:00 -0500

    compare performance of sequential and parallel mergesort

--- src/parallel/margesortTest.java
+++ src/parallel/margesortTest.java
@@ -3,24 +3,32 @@ package parallel;
 import java.util.Random;
 
 public class margesortTest {
-    public static void main(String[] args) {
-        int[] numbers = createRandomArray(10);
-        Sequential_MergeSort sequential = new Sequential_MergeSort(numbers);
-        sequential.mergesort(0, numbers.length - 1);
-        sequential.showResult();
+    private static final int NUM = 1000000;
 
-        numbers = createRandomArray(10);
+    public static void main(String[] args) {
+        int[] numbers = createRandomArray(NUM);
+        Sequential_MergeSort sequential = new Sequential_MergeSort(numbers);
+        long startTime = System.currentTimeMillis();
+        sequential.mergesort(0, numbers.length - 1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time used to sort " + NUM + " numbers using sequential mergesort: " + (endTime - startTime));
+        //sequential.showResult();
+
+        numbers = createRandomArray(NUM);
         int numThreads = Runtime.getRuntime().availableProcessors();
         Paralled_MergeSort parallel = new Paralled_MergeSort(numbers);
+        startTime = System.currentTimeMillis();
         parallel.paralledMergeSort(0, numbers.length - 1, numThreads);
-        parallel.showResult();
+        endTime = System.currentTimeMillis();
+        System.out.println("Time used to sort " + NUM + " numbers using parallel mergesort: " + (endTime - startTime));
+        //parallel.showResult();
     }
 
     public static int[] createRandomArray(int n) {
         int[] a = new int[n];
         Random random = new Random();
         for(int i = 0; i < n; i++) {
-            a[i] = random.nextInt(100000);
+            a[i] = random.nextInt(1000);
         }
         return a;
     }
